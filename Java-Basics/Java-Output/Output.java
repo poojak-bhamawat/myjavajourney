b255d24c0fc4fbbe29bc668a41e468f32bad77c3
poojak-bhamawat <you@example.com>
2026-09-14 18:23:03 +0530

    Add print number

--- Java-Basics/Java-Output/Output.java
+++ Java-Basics/Java-Output/Output.java
@@ -1,7 +1,7 @@
 public class Output{
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("I am learning Java!");
-        System.out.println("It is awesome!");
+        System.out.println(5);
+        System.out.println(3031);
+        System.out.println(3+3);
     }
 }
