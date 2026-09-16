428199c2036b0198e9898f93049ff676eb28b047
poojak-bhamawat <you@example.com>
2026-09-16 17:20:51 +0530

    Add one value to multiple variables

--- Java-Basics/Java-Variables/Variable.java
+++ Java-Basics/Java-Variables/Variable.java
@@ -5,12 +5,15 @@ public class Variable{
         String myName = "John";
         int x = 5;
         int y = 6;
-        int a = 5, b = 6, c = 7; // multiple variables in one line
+        int a = 5, b = 6, c = 7; // declare multiple variables in one line
+        int p, q, r;
+        p = q = r = 10; // one value to multiple variables in one line
         System.out.println(myText);
         System.out.println(myNum);
         System.out.println("Hello "+myName); // concatenation
         System.out.println("The sum is: "+x+y); // The sum is: 56
         System.out.println("The sum is: "+(x+y)); // The sum is: 11
         System.out.println(a+b+c); // 18
+        System.out.println(p+q+r); // 30
     }
 }
