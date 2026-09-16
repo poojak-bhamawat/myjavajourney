43e26e32a2651d5de875609e0c47e28525d6c6ca
poojak-bhamawat <you@example.com>
2026-09-16 17:08:14 +0530

    Add concatenation in strings by +

--- Java-Basics/Java-Variables/Variable.java
+++ Java-Basics/Java-Variables/Variable.java
@@ -1,6 +1,10 @@
 public class Variable{
     public static void main(String[] args) {
+        String myText = "Hello World"; // String is a datatype
         final int myNum = 9; // final means the value cannot be changed
+        String myName = "John";
+        System.out.println(myText);
         System.out.println(myNum);
+        System.out.println("Hello "+myName); // concatenation
     }
 }
