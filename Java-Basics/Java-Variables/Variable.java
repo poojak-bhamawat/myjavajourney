a4d28b14ab8472113a448f1f93300473c20797f5
poojak-bhamawat <you@example.com>
2026-09-16 17:16:06 +0530

    Declare many variables

--- Java-Basics/Java-Variables/Variable.java
+++ Java-Basics/Java-Variables/Variable.java
@@ -5,10 +5,12 @@ public class Variable{
         String myName = "John";
         int x = 5;
         int y = 6;
+        int a = 5, b = 6, c = 7; // multiple variables in one line
         System.out.println(myText);
         System.out.println(myNum);
         System.out.println("Hello "+myName); // concatenation
         System.out.println("The sum is: "+x+y); // The sum is: 56
         System.out.println("The sum is: "+(x+y)); // The sum is: 11
+        System.out.println(a+b+c); // 18
     }
 }
