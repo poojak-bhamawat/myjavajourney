0cbbb02ed9869e66a6ffac05e627810c0ce8f93b
poojak-bhamawat <you@example.com>
2026-09-16 17:12:39 +0530

    Mixing text and numbers

--- Java-Basics/Java-Variables/Variable.java
+++ Java-Basics/Java-Variables/Variable.java
@@ -3,8 +3,12 @@ public class Variable{
         String myText = "Hello World"; // String is a datatype
         final int myNum = 9; // final means the value cannot be changed
         String myName = "John";
+        int x = 5;
+        int y = 6;
         System.out.println(myText);
         System.out.println(myNum);
         System.out.println("Hello "+myName); // concatenation
+        System.out.println("The sum is: "+x+y); // The sum is: 56
+        System.out.println("The sum is: "+(x+y)); // The sum is: 11
     }
 }
