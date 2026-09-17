94508a9709c7853d554e057c4bd0efa18d2d18dd
poojak-bhamawat <you@example.com>
2026-09-17 20:05:05 +0530

    Add all Primitive and Non-Primitive data type

--- Java-Basics/Java-DataTypes/DataTypes.java
+++ Java-Basics/Java-DataTypes/DataTypes.java
@@ -1,9 +1,17 @@
 public class DataTypes{
-    public static void main(String[] args) {
+    public static void main(String[] args) {\
+        // Primitive Data Types
+        byte myNum = 100;  // Byte value from -128 to 127
+        short myNum = 5000; // Short value from -32,768 to 32,767
         int myNum = 5;               // Integer (whole number)
+        long myNum = 15000000000L;    // Long value from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
         float myFloatNum = 5.99f;  // Floating point number
+        double myDoubleNum = 5.99d; // Floating point number
         char myLetter = 'D';       // Character
         boolean myBool = true;     // Boolean
+        
+        // Non-Primitive Data Types
         String myText = "Hello";   // String
+        String[] cars = {"Volvo", "BMW", "Ford"}; // Array
     }
 }
