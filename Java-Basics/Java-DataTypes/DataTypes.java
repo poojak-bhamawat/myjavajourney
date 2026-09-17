fb6fe17b78a05b631c9361d83118b151d8b91b1f
poojak-bhamawat <you@example.com>
2026-09-17 20:11:01 +0530

    Add var keyword

--- Java-Basics/Java-DataTypes/DataTypes.java
+++ Java-Basics/Java-DataTypes/DataTypes.java
@@ -1,10 +1,10 @@
 public class DataTypes{
-    public static void main(String[] args) {\
+    public static void main(String[] args) {
         // Primitive Data Types
-        byte myNum = 100;  // Byte value from -128 to 127
-        short myNum = 5000; // Short value from -32,768 to 32,767
-        int myNum = 5;               // Integer (whole number)
-        long myNum = 15000000000L;    // Long value from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+        byte myByte = 100;  // Byte value from -128 to 127
+        short myShort = 5000; // Short value from -32,768 to 32,767
+        int myInt = 5;               // Integer (whole number)
+        long myLong = 15000000000L;    // Long value from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
         float myFloatNum = 5.99f;  // Floating point number
         double myDoubleNum = 5.99d; // Floating point number
         char myLetter = 'D';       // Character
@@ -13,5 +13,12 @@ public class DataTypes{
         // Non-Primitive Data Types
         String myText = "Hello";   // String
         String[] cars = {"Volvo", "BMW", "Ford"}; // Array
+        
+        // var keyword
+        var Var1 = 50; // myVar will be treated as an int
+        var Var2 = 5.99; // myVar will be treated as a double
+        var Var3 = "John"; // myVar will be treated as a String
+        var Var4 = 'A'; // myVar will be treated as a char
+        var Var5 = true; // myVar will be treated as a boolean
     }
 }
