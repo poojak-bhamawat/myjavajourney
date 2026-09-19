7cb375a8f709dec72c97242a1fe364e3804870df
poojak-bhamawat <you@example.com>
2026-09-20 01:06:45 +0530

    Add arithmetic operator

--- Java-Basics/Java-Operators/Operators.java
+++ Java-Basics/Java-Operators/Operators.java
@@ -1,5 +1,14 @@
 public class Operators{
     public static void main(String[] args) {
-        
+        // Arithmetic Operators
+        int a = 10;
+        int b = 5;
+        System.out.println("Addition: " + (a + b)); // Integer addition
+        System.out.println("Subtraction: " + (a - b)); // Integer subtraction
+        System.out.println("Multiplication: " + (a * b)); // Integer multiplication
+        System.out.println("Division: " + (a / b)); // Integer division
+        System.out.println("Modulus: " + (a % b)); // Remainder
+        System.out.println("Increment: " + (++a)); // Pre-increment
+        System.out.println("Decrement: " + (--a)); // Pre-decrement
     }
 }
