0a060eca505b0b7bea530e2a957d357e92df704a
poojak-bhamawat <you@example.com>
2026-09-21 23:57:23 +0530

    Add some string operations

--- Java-Basics/Java-Strings/Strings.java
+++ Java-Basics/Java-Strings/Strings.java
@@ -1,5 +1,14 @@
 public class Strings{
     public static void main(String[] args) {
-        String name = "John";
+        String name = "John Doe";
+        String city = "New York";
+
+        System.out.println(name.length());
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+        System.out.println(name.indexOf("Doe"));
+        System.out.println(name.charAt(7));
+        System.out.println(name + city);
+        System.out.println(name.concat(city));
     }
 }
