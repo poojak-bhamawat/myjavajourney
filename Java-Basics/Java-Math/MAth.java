749bf0b4c03facdade534475acddba83279e73e7
poojak-bhamawat <bhamawatpoojak2020@gmail.com>
2026-09-22 23:35:42 +0530

    Add math methods

---
+++ Java-Basics/Java-Math/MAth.java
@@ -0,0 +1,9 @@
+public class MAth{
+    public static void main(String[] args) {
+        System.out.println(Math.max(10, 20)); // 20
+        System.out.println(Math.min(10, 20));  // 10
+        System.out.println(Math.sqrt(16));  // 4.0
+        System.out.println(Math.abs(-16));  // 16
+        System.out.println(Math.pow(2, 3));  // 8.0
+    }
+}
