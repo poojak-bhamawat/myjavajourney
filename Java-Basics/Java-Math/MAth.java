be053d5d01731165d4cafde65bfa1cf56d23399e
poojak-bhamawat <bhamawatpoojak2020@gmail.com>
2026-09-22 23:38:31 +0530

    Add rounding methods

--- Java-Basics/Java-Math/MAth.java
+++ Java-Basics/Java-Math/MAth.java
@@ -1,9 +1,16 @@
 public class MAth{
     public static void main(String[] args) {
+        // Math methods
         System.out.println(Math.max(10, 20)); // 20
         System.out.println(Math.min(10, 20));  // 10
         System.out.println(Math.sqrt(16));  // 4.0
         System.out.println(Math.abs(-16));  // 16
         System.out.println(Math.pow(2, 3));  // 8.0
+
+        // Rounding methods
+        System.out.println(Math.round(4.4));  // 4
+        System.out.println(Math.round(4.6));  // 5
+        System.out.println(Math.ceil(4.4));  // 5
+        System.out.println(Math.floor(4.4));  // 4
     }
 }
