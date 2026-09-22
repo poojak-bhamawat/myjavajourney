b6758d7c4f95f18dc84cf46c54fc4669eb59c793
poojak-bhamawat <bhamawatpoojak2020@gmail.com>
2026-09-22 23:25:43 +0530

    add strings and numbers

--- Java-Basics/Java-Strings/Strings.java
+++ Java-Basics/Java-Strings/Strings.java
@@ -10,5 +10,18 @@ public class Strings{
         System.out.println(name.charAt(7));
         System.out.println(name + city);
         System.out.println(name.concat(city));
+
+        // Numbers and Strings
+        int x = 10;
+        int y = 20;
+        int z = x + y; //z will be 30(an integer)
+
+        String a = "10";
+        String b = "20";
+        String c = a + b; //c will be "1020"(a string)
+
+        String p = "10";
+        int q = 20;
+        String r = p + q; //r will be "1020"(a string)
     }
 }
