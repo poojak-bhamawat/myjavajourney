b973417822d345fe895727b78eb9320cd0ffda97
poojak-bhamawat <bhamawatpoojak2020@gmail.com>
2026-09-23 23:17:48 +0530

    Add if condition

--- Java-Basics/Java-If...Else/IfElse.java
+++ Java-Basics/Java-If...Else/IfElse.java
@@ -1,5 +1,13 @@
 public class IfElse{
     public static void main(String[] args) {
-        
+        // Using boolean variable
+        boolean isRaining = true;
+        if(isRaining){
+            System.out.println("Take an umbrella"); 
+        }
+
+        // If without curly braces
+        if (20 > 18)
+            System.out.println("20 is greater than 18"); 
     }
 }
