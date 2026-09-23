bd2e731e2c8b82a6cb1d17cce2d1b4087f64497b
poojak-bhamawat <bhamawatpoojak2020@gmail.com>
2026-09-23 23:21:11 +0530

    Add else condition

--- Java-Basics/Java-If...Else/IfElse.java
+++ Java-Basics/Java-If...Else/IfElse.java
@@ -9,5 +9,13 @@ public class IfElse{
         // If without curly braces
         if (20 > 18)
             System.out.println("20 is greater than 18"); 
+
+        // Using else
+        int age = 22;
+        if (age > 18){
+            System.out.println("You are eligible to vote");
+        } else {
+            System.out.println("You are not eligible to vote");
+        }
     }
 }
